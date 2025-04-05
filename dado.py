6395929bfebb0125cf3a46a9c4f45c1544a11da3
Her01naFazBem <143205687+Her01naFazBem@users.noreply.github.com>
2025-04-05 19:08:13 -0300

    Add files via upload

---
+++ dado.py
@@ -0,0 +1,38 @@
+# Classe dado 05/04/2025 - Camila
+
+from abc import ABC, abstractmethod
+from random import randint
+
+class Dado (ABC):
+    def __init__(self, lados : int):
+        if lados > 0 :
+            self._lados = lados
+        else:
+            raise ValueError("O dado deve conter um número positivo de lados")
+    
+    def jogar (self) -> int:
+        return randint (1, self._lados)
+    
+class D4 (Dado):
+    def __init__(self):
+        super().__init__(4)
+    
+class D6 (Dado):
+    def __init__(self):
+        super().__init__(6)
+    
+class D8 (Dado):
+    def __init__(self):
+        super().__init__(8)
+    
+class D10 (Dado):
+    def __init__(self):
+        super().__init__(10)
+    
+class D12 (Dado):
+    def __init__(self):
+        super().__init__(12)
+    
+class D20 (Dado):
+    def __init__(self):
+        super().__init__(20)
