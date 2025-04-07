29f46420a486fa623c38f35095bae5d381309dfd
Her01naFazBem <143205687+Her01naFazBem@users.noreply.github.com>
2025-04-07 10:15:24 -0300

    Add files via upload

--- personagem.py
+++ personagem.py
@@ -8,9 +8,30 @@ class Personagem:
         self._classe = classe
         self._inventario: list[Habilidade] = []
         self._pontos_vida = classe._pontos_vida
-        self._dano_de_ataque = classe._dado_ataque
+        self._dado_de_ataque = classe._dado_ataque
         self._pontos_de_defesa = classe._pontos_defesa
         self._pontos_de_ataque = classe._pontos_ataque
         Personagem.qntd_instancias += 1
 
+    def __str__(self):
+        return (
+            f"Nome: {self._nome}\n"
+            f"Classe: {self._classe}\n"
+            f"Inventário: {self._inventario}\n"
+            f"Pontos de Vida: {self.pontos_vida}\n"
+            f"Dado de Ataque: {self._dado_de_ataque}\n"
+            f"Pontos de Defesa: {self._pontos_de_defesa}\n"
+            f"Pontos de Ataque: {self._pontos_de_ataque}\n"    
+            )
     
+    def __repr__(self):
+        return (
+            f"Nome = {self._nome}\n"
+            f"Classe = {self._classe}\n"
+            f"Inventário = {self._inventario}\n"
+            f"Pontos de Vida = {self.pontos_vida}\n"
+            f"Dado de Ataque = {self._dado_de_ataque}\n"
+            f"Pontos de Defesa = {self._pontos_de_defesa}\n"
+            f"Pontos de Ataque = {self._pontos_de_ataque}\n"    
+            )
+                
